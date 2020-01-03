f765d0999650b7c39d92d01ba3027937049a9d0d
wangxingxiang <wangxingxiang@111.com.cn>
2020-01-03 11:37:56 +0800

    m

--- sv2tts/parser_textgrid.py
+++ sv2tts/parser_textgrid.py
@@ -1,5 +1,6 @@
 import textgrid
 import os
+from tqdm import tqdm
 
 def parse_Interval(IntervalObject):
     start_time = ""
@@ -75,28 +76,46 @@ def parse_textgrid(filename):
 
     for ele in words_list:
         d = parse_Interval(ele)
-        print(d)
+        #print(d)
 
 def parse_textgrid_line(filename):
     tg = textgrid.TextGrid.fromFile(filename)
     list_words = tg.getList("words")
     words_list = list_words[0]
 
-    word = ','
+    word = ''
     time = ''
+    index = 0
     for ele in words_list:
         w,t = parse_Interval_key(ele)
+
+        if w=='None':
+            if index != len(words_list)-1:
+                word = word + ''.join('')+','
+                time = time + ''.join(str(t[1])) + ',' 
+            else:
+                time = time + ''.join(str(t[1]))
+
         if w!='None':
             word = word + ''.join(w)+','
-        time = time + ''.join(str(t[1])) + ',' 
+            time = time + ''.join(str(t[1])) + ',' 
+        index = index + 1
 
     #print(word) 
     #print(time) 
+    contend = os.path.split(filename)[-1].split(".")[0] + ' "'+ word + '" "' + time + ''
+    utterance_id, words, end_times = contend.strip().split(' ')
+    words = words.replace('\"', '').split(',')
+    end_times = [float(e) for e in end_times.replace('\"', '').split(',')]
+    #print(len(words)) 
+    #print(len(end_times)) 
+    assert len(words) == len(end_times)
+
     return os.path.split(filename)[-1].split(".")[0], word, time
 
 def parse(file_path, o_post):
     ls = os.listdir(file_path)
-    for i in ls:
+    for i in tqdm(ls):
         son_path = os.path.join(file_path,i)
         if os.path.isdir(son_path):
             parse(son_path,o_post)
@@ -105,10 +124,10 @@ def parse(file_path, o_post):
             # o_post file
             if file_post == o_post:
                 #os.rename(son_path,str(son_path.split('.')[0])+'.'+n_post)
-                print('找到文件{srcnam}'.format(srcnam=son_path))
+                #print('找到文件{srcnam}'.format(srcnam=son_path))
                 contend = parse_textgrid_line(son_path)
                 
-                filename = '20170001P00001.alignment.txt'
+                filename = 'ST-CMDS.alignment.txt'
                 
                 with open(filename, 'a', encoding='UTF-8') as file_object:
                     contends = file_object.writelines(contend[0] + ' "'+ contend[1] + '" "' + contend[2] + '"\n')
@@ -116,5 +135,9 @@ def parse(file_path, o_post):
 if __name__ == "__main__":
     #parse_Interval('Interval(13.63000, 13.78000, ah0)')
     #parse_textgrid("D:/dataset/aligned_output/ST-CMDS-20170001_1-OS/20170001P00001A0001.TextGrid")
-    print(parse_textgrid_line("/media/mungo/新加卷/ST-CMDS-20170001_1-OS/20170001P00444A0120.TextGrid"))
-    #parse("D:/dataset/aligned_output/ST-CMDS-20170001_1-OS","TextGrid")
+    # 行形式
+    #print(parse_textgrid_line("D:/dataset/ST-CMDS-TextGrid/20170001P00001A0008.TextGrid"))
+    # 详细信息
+    #print(parse_textgrid("D:/dataset/ST-CMDS-TextGrid/20170001P00001A0008.TextGrid"))
+    # 生成强制对齐文件
+    parse("D:/dataset/ST-CMDS-TextGrid","TextGrid")
